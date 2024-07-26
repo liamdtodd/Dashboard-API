eca1bc2db182cdb7c28e3028f8c1fc2c90aae2e6
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-07-25 21:01:04 -0700

    route added for authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const router = module.exports = require('express').Router();
 
+router.use('/', require('./auth/auth'));
 router.use('/user', require('./collections/user'));
 router.use('/calendar', require('./collections/calendar'));
 router.use('/note', require('./collections/note'));
